d5f5dae4d9d2cbc786cf1341bd0fd5defbe0ebb8
Bᴇʀɴᴅ Sᴄʜᴏʀɢᴇʀs <me@bjw-s.dev>
2022-07-12 09:49:31 +0200

    feat: Run `fsfreeze --unfreeze` on script exit

--- script/backup.sh
+++ script/backup.sh
@@ -6,20 +6,17 @@ FILE="$(date "+%Y-%m-%d_%H_%M_%S").tar.gz"
 
 echo "INFO Backing up ${PMB__SOURCE_DIR} contents to ${FILE}"
 
-if [[ "${PMB__FSFREEZE}" == "true" ]]; then
-  fsfreeze --freeze "${PMB__SOURCE_DIR}"
-fi
-
 excludeArgs=()
 for pattern in ${PMB__EXCLUDE_PATTERNS}; do
   excludeArgs+=("--exclude=${pattern}")
 done
-tar "${excludeArgs[@]}" -zcf - -C "${PMB__SOURCE_DIR}" . | rclone rcat "${PMB__RCLONE_REMOTE}:${PMB__RCLONE_REMOTE_PATH}${FILE}" --config "${PMB__RCLONE_CONFIG}"
 
 if [[ "${PMB__FSFREEZE}" == "true" ]]; then
-  # TODO: This needs to _always_ run if "${PMB__FSFREEZE}" == "true", perhaps use `trap`?
-  fsfreeze --unfreeze "${PMB__SOURCE_DIR}"
+  trap 'fsfreeze --unfreeze ${PMB__SOURCE_DIR}' EXIT
+  fsfreeze --freeze "${PMB__SOURCE_DIR}"
 fi
+
+tar "${excludeArgs[@]}" -zcf - -C "${PMB__SOURCE_DIR}" . | rclone rcat "${PMB__RCLONE_REMOTE}:${PMB__RCLONE_REMOTE_PATH}${FILE}" --config "${PMB__RCLONE_CONFIG}"
 
 echo "INFO Cleaning files older than ${PMB__KEEP_DAYS} days..."
 total_backups=$(rclone size "${PMB__RCLONE_REMOTE}:${PMB__RCLONE_REMOTE_PATH}" --config "${PMB__RCLONE_CONFIG}" --json | jq --raw-output '.count')
